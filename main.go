90a5792a4d5c9edd4b5e4c4e8c4d3813c6870b3b
JY <284703576@qq.com>
2023-03-17 18:03:03 +0800

    alter paginator

--- main.go
+++ main.go
@@ -69,19 +69,22 @@ func getList(total uint, size uint, slot uint, currentPage uint) []string {
 func getList2(total uint, size uint, slot uint, currentPage uint) []string {
 	var lists []string
 	pageCount := uint(math.Ceil(float64(total) / float64(size)))
+	if currentPage > pageCount {
+		currentPage = pageCount
 
+	}
 	if pageCount <= slot {
+		fmt.Println("case1")
 		for i := 1; i <= int(pageCount); i++ {
 			lists = append(lists, fmt.Sprint(i))
 		}
 		return lists
 
 	}
-
 	space := (slot - 1) / 2
-
-	if currentPage < slot {
-		for i := 1; i < int(slot+space); i++ {
+	if currentPage <= space {
+		fmt.Println("case2")
+		for i := 1; i < int(slot-1); i++ {
 			lists = append(lists, fmt.Sprint(i))
 		}
 		lists = append(lists, "...")
@@ -89,17 +92,19 @@ func getList2(total uint, size uint, slot uint, currentPage uint) []string {
 		return lists
 
 	}
-	if currentPage > pageCount-slot {
+	if currentPage > pageCount-(slot-2) {
+		fmt.Println("case3")
 		lists = append(lists, fmt.Sprint(1))
 		lists = append(lists, "...")
-		for i := int(pageCount - slot); i <= int(pageCount); i++ {
+		for i := int(pageCount - (slot - 3)); i <= int(pageCount); i++ {
 			lists = append(lists, fmt.Sprint(i))
 		}
 		return lists
 	}
+	fmt.Println("case4")
 	lists = append(lists, fmt.Sprint(1))
 	lists = append(lists, "...")
-	for i := int(currentPage - ((slot - 1) / 2)); i <= int(currentPage+((slot-1)/2)); i++ {
+	for i := int(currentPage - (space - 2)); i <= int(currentPage+(space-2)); i++ {
 		lists = append(lists, fmt.Sprint(i))
 	}
 	lists = append(lists, "...")
@@ -110,6 +115,6 @@ func getList2(total uint, size uint, slot uint, currentPage uint) []string {
 
 func main() {
 
-	fmt.Println(getList2(80, 10, 7, 2))
+	fmt.Println(getList2(80, 10, 7, 5))
 
 }
--- tool/pagination/pagination.go
+++ tool/pagination/pagination.go
@@ -1,42 +1,113 @@
 package pagination
 
 import (
+	"fmt"
 	"math"
+	"strings"
 )
 
-type Pagination struct {
-	Total uint
-	Size  uint
-	Page  uint
-	Path  string
-	Slot  uint
+type Paginator struct {
+	Total       int
+	PerPage     int
+	CurrentPage int
+	Path        string
+	EachSide    int
 }
 
-func Defalut() *Pagination {
-
-	return &Pagination{
-		Total: 100,
-		Size:  10,
-		Path:  "/",
-	}
+type PageUrl struct {
+	Page string
+	Url  string
 }
 
-func (p *Pagination) method() {
+func (p *Paginator) method() {
 
 }
 
-func (p *Pagination) getPageCount() uint {
-	if p.Total == 0 {
-		return 1
-	}
-	return uint(math.Ceil(float64(p.Total) / float64(p.Size)))
-}
-
-func (p *Pagination) getList() {
-	pageCount := p.getPageCount()
-	sort := p.Slot
-	if pageCount <= sort+2 {
-
+func (p *Paginator) getCount() (count int) {
+	if p.Total > 0 {
+		count = int(math.Ceil(float64(p.Total) / float64(p.getPerPage())))
 	}
 
+	return
+}
+
+func (p *Paginator) GetLists() (lists []string) {
+	count := p.getCount()
+	eachSide := p.EachSide
+	currentPage := p.CurrentPage
+	if currentPage > count {
+		currentPage = count
+	}
+	if count <= 2*eachSide+1 {
+		fmt.Println("case1")
+		for i := 1; i <= count; i++ {
+			lists = append(lists, fmt.Sprint(i))
+		}
+		return
+	}
+	if currentPage <= eachSide {
+		fmt.Println("case2")
+		for i := 1; i < 2*eachSide; i++ {
+			lists = append(lists, fmt.Sprint(i))
+		}
+		lists = append(lists, "...")
+		lists = append(lists, fmt.Sprint(count))
+		return
+	}
+	if currentPage > count-eachSide-1 {
+		fmt.Println("case3")
+		lists = append(lists, fmt.Sprint(1))
+		lists = append(lists, "...")
+		for i := count - eachSide - 1; i <= count; i++ {
+			lists = append(lists, fmt.Sprint(i))
+		}
+		return
+	}
+	fmt.Println("case4")
+	lists = append(lists, fmt.Sprint(1))
+	lists = append(lists, "...")
+	for i := currentPage - (eachSide - 2); i <= currentPage+(eachSide-2); i++ {
+		lists = append(lists, fmt.Sprint(i))
+	}
+	lists = append(lists, "...")
+	lists = append(lists, fmt.Sprint(count))
+
+	return
+}
+
+func (p *Paginator) getPerPage() int {
+	if p.PerPage > 100 {
+		return 100
+	}
+	return p.PerPage
+
+}
+
+func (p *Paginator) isFirstPage(page string) bool {
+	return page == "1"
+}
+
+func (p *Paginator) getUrl(page string) string {
+	if p.isFirstPage(page) {
+		return p.Path
+	}
+	if strings.Contains(p.Path, "?") {
+		return p.Path + "&page=" + page
+	} else {
+		return p.Path + "?page=" + page
+	}
+
+}
+
+func (p *Paginator) GetPageUrls() (pageUrls []PageUrl) {
+	lists := p.GetLists()
+	for _, list := range lists {
+		var pageUrl PageUrl
+		pageUrl.Page = list
+		if list != "..." {
+			p.getUrl(list)
+		}
+		pageUrls = append(pageUrls, pageUrl)
+	}
+	return
 }
