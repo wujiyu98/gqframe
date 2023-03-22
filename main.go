48171050007e8dd480595b16aabd03612c57eaad
JY <284703576@qq.com>
2023-03-22 09:50:54 +0000

    alter email

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+import "github.com/wujiyu98/gqframe/tool/email"
+
 func main() {
+	email.Send()
 
 }
--- tool/email/email.go
+++ tool/email/email.go
@@ -26,8 +26,9 @@ func Send() {
 	m := gomail.NewMessage()
 	m.SetHeader("From", username)
 	m.SetHeader("To", toMail)
-	m.SetHeader("Subject", "没有一个中文的邮件看下")
-	m.SetBody("text/plain", "你好，这是一个测试邮箱")
+	m.SetHeader("Reply-To", "2030716640@qq.com")
+	m.SetHeader("Subject", "你好没有一个中文的邮件看下3")
+	m.SetBody("text/html", "你好，这是一个测试邮箱，添加多点文字")
 
 	d := gomail.NewDialer(host, port, username, password)
 
