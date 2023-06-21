2608d0c385180d9e0f81acf0cbc965c48ee74545
JY <284703576@qq.com>
2023-06-21 11:47:36 +0800

    alter main.go

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
 func main() {
 	// email.Send()
-	price := 1234567890
-	priceStr := fmt.Sprintf("%d", price)
-	priceStr = priceStr[:-1]
-	priceStr = priceStr[1:]
-	priceStr = strings.Replace(priceStr, ".", ",", -1)
-	fmt.Println(priceStr)
 
 }
